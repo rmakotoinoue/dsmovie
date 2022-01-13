e336d21fff8aa9185f5a4a970ff540262d92bcc3
rmakotoinoue <rmakotoinoue@gmail.com>
2022-01-13 09:25:00 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -16,10 +16,17 @@ public class MovieService {
 	@Autowired
 	private MovieRepository repository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		Page<Movie> result = repository.findAll(pageable);
 		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
 		return page;
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
 	}
 }
